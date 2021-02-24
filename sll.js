f0ea2f06fcf4611d026a24142e715bd6cf8822fc
Johnny <jaoldaker1@gmail.com>
2021-02-23 20:35:46 -0500

    starting linked list

---
+++ sll.js
@@ -0,0 +1,45 @@
+class SLL{
+    constructor(){
+        this.head = null
+    }
+    addFront(value){
+        var new_node = new Node(value)
+        new_node.next = this.head
+        this.head = new_node
+        return this.head
+    }
+    removeFront(){
+        this.head = this.head.next
+        return this.head
+    }
+    front(){
+        return this.head.value
+    }
+    view(){
+        var runner = this.head;
+        while(runner){
+            console.log(`Value: ${runner.value}`)
+            runner = runner.next
+        }
+    }
+}
+
+
+class Node{
+    constructor(value){
+        this.value = value
+        this.next = null
+    }
+}
+
+new_SLL = new SLL()
+new_Node = new Node(10)
+new_SLL.head = new_Node
+new_SLL.addFront(2)
+new_SLL.addFront(4)
+new_SLL.addFront(6)
+new_SLL.view()
+new_SLL.removeFront()
+console.log("")
+new_SLL.view()
+console.log(new_SLL.front())
